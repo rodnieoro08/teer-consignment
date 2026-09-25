9524b33a9aa29ed2e66c018a354e8b9fd8a3cbc6
rodnieoro08 <rodnieoro@gmail.com>
2026-09-25 10:13:11 +0100

    Bump cache to v3 and prefer network so scanner fix reaches phones

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "teer-consign-v2";
+const CACHE = "teer-consign-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./cloud.js", "./data.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
@@ -7,5 +7,5 @@ self.addEventListener("activate", (e) => {
   e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
 });
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
