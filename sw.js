6d010debcf48f827105ece5cc7a8e3b11f6455ac
rodnieoro08 <rodnieoro@gmail.com>
2026-09-25 10:14:43 +0100

    Cache scan-fix.js so phones pick up the dual-barcode reader

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "teer-consign-v3";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./cloud.js", "./data.js", "./manifest.json", "./icon.svg"];
+const CACHE = "teer-consign-v4";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./scan-fix.js", "./cloud.js", "./data.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
